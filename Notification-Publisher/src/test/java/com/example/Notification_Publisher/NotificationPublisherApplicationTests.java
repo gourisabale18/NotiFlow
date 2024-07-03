bd92a43b9550820ea836adca309ddaca0f6a2a98
gsabale <sabalegouri1996@gmail.com>
2024-07-02 22:45:29 -0700

    Maven Project created using STS with resources and java code

---
+++ Notification-Publisher/src/test/java/com/example/Notification_Publisher/NotificationPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Notification_Publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
